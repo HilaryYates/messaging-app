dc4c181a4a987dbe4682fa607da2b232e6e1ef92
hilaryyates <hilryyates@gmail.com>
2023-04-12 12:54:20 -0400

    socket connection established

---
+++ chat app/server/index.tsx
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`user connected ${socket.id}`);
+
+  socket.on("disconnect", () => {
+    console.log(`user disconnected ${socket.id}`);
+  });
+});
+
+server.listen(5000, () => console.log("server running"));
